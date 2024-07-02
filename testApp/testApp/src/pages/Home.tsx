bc40bd4403dd546a1171e0f8972b013ac39e1f83
DigvijayDutt <duttdigvijay@gmail.com>
2024-07-02 22:42:40 +0530

    home page css

--- testApp/testApp/src/pages/Home.tsx
+++ testApp/testApp/src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import Navbar from '../components/Navbar';
-
+import './body.css';
+import './hp_care.css';
+import './hp_pet.css';
 function Home(){
   return (
     <> 
       <Navbar />
-      
+      <header className='header'>
+        <div className='left'>
+          
+        </div>
+      </header>
     </>
   );
 }
